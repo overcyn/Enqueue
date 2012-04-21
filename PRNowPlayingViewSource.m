33cabd22d3b8a299c7e2b17fc58a0078071a3171
Kevin Dang <overcyn@gmail.com>
2012-04-21 10:30:42 -0700

    use compilations in nowplayingview

--- PRNowPlayingViewSource.m
+++ PRNowPlayingViewSource.m
@@ -8,10 +8,8 @@
 #pragma mark - Initialization
 
 - (id)initWithDb:(PRDb *)db_ {
-    self = [super init];
-	if (self) {
-		_db = db_;
-	}
+	if (!(self = [super init])) {return nil;}
+    _db = db_;
 	return self;
 }
 
@@ -19,12 +17,9 @@
 
 }
 
-- (BOOL)initialize {	
-    NSString *string = @"CREATE TEMP TABLE now_playing_view_source ("
-    "row INTEGER NOT NULL PRIMARY KEY, "
-    "file_id INTEGER NOT NULL "
-    ")";
-    [_db execute:string];
+- (BOOL)initialize {
+    [_db execute:@"CREATE TEMP TABLE now_playing_view_source ("
+     "row INTEGER NOT NULL PRIMARY KEY, file_id INTEGER NOT NULL)"];
     return TRUE;
 }
 
@@ -63,38 +58,35 @@
 - (NSArray *)albumCounts {
     NSString *string;
     if ([[PRUserDefaults userDefaults] useAlbumArtist]) {
-        string = @"SELECT library.album, library.artistAlbumArtist "
-        "FROM now_playing_view_source "
-        "JOIN library ON now_playing_view_source.file_id = library.file_id";
+        string = @"SELECT library.album, library.artistAlbumArtist, library.compilation "
+        "FROM now_playing_view_source JOIN library ON now_playing_view_source.file_id = library.file_id";
 	} else {
-        string = @"SELECT library.album, library.artist "
-        "FROM now_playing_view_source "
-        "JOIN library ON now_playing_view_source.file_id = library.file_id";
+        string = @"SELECT library.album, library.artist, library.compilation "
+        "FROM now_playing_view_source JOIN library ON now_playing_view_source.file_id = library.file_id";
     }
-    NSArray *results = [_db execute:string bindings:nil columns:@[PRColString, PRColString]];
-    
+    NSArray *results = [_db execute:string bindings:nil columns:@[PRColString, PRColString, PRColInteger]];
     if ([results count] == 0) {
         return @[];
-    } else if ([results count] == 1) {
-        return @[@1];
     }
     
     NSMutableArray *array = [NSMutableArray array];
     int count = 1;
-    
     for (int i = 0; i < [results count] - 1; i++) {
         NSString *albumString = [[results objectAtIndex:i] objectAtIndex:0];
         NSString *artistString = [[results objectAtIndex:i] objectAtIndex:1];
+        BOOL compilation = [[[results objectAtIndex:i] objectAtIndex:2] boolValue];
         NSString *albumString2 = [[results objectAtIndex:i + 1] objectAtIndex:0];
         NSString *artistString2 = [[results objectAtIndex:i + 1] objectAtIndex:1];
-        if ([albumString noCaseCompare:albumString2] != NSOrderedSame ||
-            [artistString noCaseCompare:artistString2] != NSOrderedSame) {
-                [array addObject:[NSNumber numberWithInt:count]];
-                count = 0;
-            }
+        BOOL compilation2 = [[[results objectAtIndex:i + 1] objectAtIndex:2] boolValue];
+        
+        BOOL albumSame = [albumString noCaseCompare:albumString2] == NSOrderedSame;
+        BOOL artistSame = [artistString noCaseCompare:artistString2] == NSOrderedSame;
+        if (compilation != compilation2 || (compilation && !albumSame) || (!compilation && (!albumSame || !artistSame))) {
+            [array addObject:[NSNumber numberWithInt:count]];
+            count = 0;
+        }
         count++;
     }
-    
     [array addObject:[NSNumber numberWithInt:count]];
     return array;
 }
